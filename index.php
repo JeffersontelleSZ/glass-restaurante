1d874ac6ae433e69b44a35963f02d079ec72fa33
Jefferson Teles <jefferson_dev@outlook.com>
2019-09-10 14:18:19 -0300

    inicialização do projeto, configuracoes sass, section one

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Open+Sans:300,400,700&display=swap" rel="stylesheet" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css">
+  <title>Glass Restaurante</title>
+</head>
+
+<body>
+
+  <header class="header-top">
+    <div class="container">
+      <div class="row text-center">
+        <div class="col-md-12">
+          <h1 class="header-logo"><span>Glass</span> Restaurantes</h1>
+          <p class="header-sub">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Navbar -->
+  <section class="sticky-top">
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarMenu">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarMenu">
+          <ul class="navbar-nav mx-auto">
+            <li class="navbar-item"><a href="#" class="nav-link">Inicio</a></li>
+            <li class="navbar-item"><a href="#" class="nav-link">Sobre nos</a></li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Menu
+              </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Almoço</a>
+                <a class="dropdown-item" href="#">Jantar</a>
+                <a class="dropdown-item" href="#">Sobremesas</a>
+              </div>
+            </li>
+            <li class="navbar-item"><a href="#" class="nav-link">Eventos</a></li>
+            <li class="navbar-item"><a href="#" class="nav-link">Contatos</a></li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  </section>
+
+  <!-- Section One -->
+  <section class="container">
+    <div class="section-one">
+      <div class="row text-right">
+        <div class="offset-md-7 col-5">
+          <div class="section-one-content">
+            <h2 class="heading-lg">Delicious meat:</h2>
+            <span class='heading-italic'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, sapiente earum. Consequuntur ratione, nostrum eveniet illum odio necessitatibus nulla perferendis.</span>
+            <p class="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere hic laudantium aut cupiditate molestias perspiciatis corrupti dolorem officiis quo natus, ut vitae optio maiores voluptas earum, repudiandae voluptatibus? Itaque, deleniti?</p>
+
+            <hr class="line">
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+
+</html>
